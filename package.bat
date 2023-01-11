b5d5b9ef01a00a8fe88ae6170ed6ae6639c751c9
Andreas <draeger@informatik.uni-tuebingen.de>
2023-01-11 17:37:40 +0100

    Added a script to package the app for Windows

---
+++ package.bat
@@ -0,0 +1,1 @@
+javapackager -deploy -title "MassMailTool" -name "MassMailTool" -appclass org.draeger.massmailtool.MassMailTool -native exe -outdir target -outfile MassMailTool -srcfiles target/massmailtool-0.1-full.jar
